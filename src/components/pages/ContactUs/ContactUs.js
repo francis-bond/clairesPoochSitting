115b4795cd3739b603073405f7a6ba345a15e5d5
Francis Bond <francisbond1996@gmail.com>
2023-07-08 18:53:19 -0500

    Added last section of content

--- src/components/pages/ContactUs/ContactUs.js
+++ src/components/pages/ContactUs/ContactUs.js
@@ -1,12 +1,28 @@
 import React from "react";
+import "./ContactUs.css"
 
 export default function ContactUs(){
     return(
     <body className="w-100 h-auto">
         <div className="">
             <h2 className="ml-5 p-2">Contact Us</h2>
-            <p className="ml-4">If you're like us, your pets have become family members. When you can't be home for them, whether because of work, vacation or other obligations, you want them taken care of with the same love and attention you give them. That's exactly what we do at Claire's Pooch Sitting. Because we love pets!</p>
-            <p className="ml-4">Claire's Pooch Sitting offers a multitude of services for a variety of pets. We can also handle various home services which, by making it appear that your house is occupied, can be a crime deterrent. Choose the schedule and services you want, and we'll do the rest.</p>
+            <h4 className="ml-5 p-2">Contact information</h4>
+            <div className="">
+            <p className="ml-4 font-weight-bold">Arlette Bond</p>
+            <p className="ml-4">Phone: 832-726-6775</p>
+            <p className="ml-4">Email: clairespoochsitting@gmail.com</p>
+            </div>
+            <div className="">
+            <p className="ml-4 font-weight-bold">Hours of Operation</p>
+            <p className="ml-4">Mon-Fri: 7am-9pm</p>
+            <p className="ml-4">Sat: 7am-9pm</p>
+            <p className="ml-4">Sun: 7am-9pm</p>
+            </div>
+            <div className="">
+            <p className="ml-4 font-weight-bold">Address</p>
+            <p className="ml-4">Alden Bridge area</p>
+            <p className="ml-4">The Woodlands, TX 77382</p>
+            </div>
         </div>
     </body>
     )
