c79e6f1e1f09191e19659206e7d12009c3e67f1a
Francis Bond <francisbond1996@gmail.com>
2023-07-08 20:23:31 -0500

    added close toggle to nav items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,10 @@ import "./Header.css"
 export default function Header(){
     return(
     <header className="w-100">
-        <div className="text-center pt-5">
+        <div className="text-center pt-4 pb-0">
             <h1>Claire's Pooch Sitting</h1>
         </div>
-        <div className="text-center tagline">
-            <h4>Precious to me as they are to you</h4>
-        </div>
+            <h4 className="text-center tagline pb-0">Precious to me as they are to you</h4>
         <div className="text-center">
             <img src={mumslogo} className="img-fluid mx-auto" alt="cartoon dogs on a leash"/>
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,6 +13,7 @@ function Navbar({ currentPage, handlePageChange }) {
             <a
               href="#home"
               onClick={() => handlePageChange('Home')}
+              type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
               // This is a conditional (ternary) operator that checks to see if the current page is "Home"
               // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
               className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
@@ -25,6 +26,7 @@ function Navbar({ currentPage, handlePageChange }) {
               href="#aboutus"
               onClick={() => handlePageChange('AboutUs')}
               className={currentPage === 'AboutUs' ? 'nav-link active' : 'nav-link'}
+              type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
             >
               About Us
             </a>
@@ -35,6 +37,7 @@ function Navbar({ currentPage, handlePageChange }) {
               onClick={() => handlePageChange('ContactUs')}
               // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
               className={currentPage === 'ContactUs' ? 'nav-link active' : 'nav-link'}
+              type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
             >
               Contact Us
             </a>
@@ -45,6 +48,7 @@ function Navbar({ currentPage, handlePageChange }) {
               onClick={() => handlePageChange('Services')}
               // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
               className={currentPage === 'Services' ? 'nav-link active' : 'nav-link'}
+              type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
             >
               Services
             </a>
